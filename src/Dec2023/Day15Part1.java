4b4911defa8314be8a412df0cccf39fcb559e2d7
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2023-12-15 15:27:36 +0000

    Day 15

---
+++ src/Dec2023/Day15Part1.java
@@ -0,0 +1,51 @@
+package Dec2023;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day15Part1 {
+	
+	private static String[] stringsList;
+	
+	public static void run() {
+		initializeStringsList();
+		
+		int sum = 0;
+		
+		for (String s : stringsList) {
+			sum += calculateHashValue(s);
+		}
+		
+		System.out.println(sum);		
+	}
+	
+	private static int calculateHashValue(String s) {
+		int total = 0;
+		
+		for (int i = 0; i < s.length(); i++) {
+			total += (int) s.charAt(i);
+			total *= 17;
+			total = total % 256;
+		}
+		
+		return total;
+	}
+	
+	private static void initializeStringsList() {
+		try {
+		    File myObj = new File("InputFiles/15.txt");
+		    Scanner myReader = new Scanner(myObj);
+		    while (myReader.hasNextLine()) {	        	
+		        String line = myReader.nextLine();
+		        stringsList = line.split(",");
+		    }
+		    
+		    myReader.close();
+		}
+		catch (FileNotFoundException e) {
+		    e.printStackTrace();
+		}
+	}
+
+}
--- src/Dec2023/Run.java
+++ src/Dec2023/Run.java
@@ -3,7 +3,7 @@ package Dec2023;
 public class Run {
 	
 	public static void main(String[] args) {
-		Day14Part2.run();
+		Day15Part2.run();
 	}
 
 }
