e7665282d7e372357420fca8ed1d764b2dcad456
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2024-12-07 19:06:59 +0000

    2024 Day 7

---
+++ src/Dec2024/Day07Part1.java
@@ -0,0 +1,62 @@
+package Dec2024;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import main.Challenge;
+
+public class Day07Part1 extends Challenge {
+    
+    private long solve(List<Long> results, List<List<Long>> numbers) {
+        int n = results.size();
+        long total = 0;
+        for (int i = 0; i < n; i++) {
+            if (isPossibleEquation(results.get(i), numbers.get(i))) {
+                total += results.get(i);
+            }
+        }
+        
+        return total;
+    }
+    
+    private boolean isPossibleEquation(long result, List<Long> numbers) {
+        return isPossibleEquation(result, numbers, 1, numbers.get(0));
+    }
+    
+    private boolean isPossibleEquation(long result, List<Long> numbers, int next, long current) {
+        if (next == numbers.size()) {
+            return result == current;
+        }
+        
+        return isPossibleEquation(result, numbers, next + 1, current + numbers.get(next)) || 
+                isPossibleEquation(result, numbers, next + 1, current * numbers.get(next));
+    }
+
+    @Override
+    public long solve() {
+        List<Long> results = new ArrayList<>();
+        List<List<Long>> numbers = new ArrayList<>();
+        
+        Pattern pattern = Pattern.compile("\\d+");
+        
+        Scanner reader = getInputFile();
+        while (reader.hasNextLine()) {
+            String line = reader.nextLine();
+            Matcher matcher = pattern.matcher(line);
+            matcher.find();
+            results.add(Long.parseLong(matcher.group()));
+            List<Long> l = new ArrayList<>();
+            while (matcher.find()) {
+                l.add(Long.parseLong(matcher.group()));
+            }
+            numbers.add(l);
+        }
+        reader.close();
+        
+        return solve(results, numbers);
+    }
+    
+}
