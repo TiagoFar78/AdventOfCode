f852206ebfa0aaa91652db047fe5fa1cb94a6089
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2025-02-09 17:37:22 +0000

    add: 2015 Day 7

---
+++ src/year2015/Day07Part1.java
@@ -0,0 +1,80 @@
+package year2015;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import main.Challenge;
+
+public class Day07Part1 extends Challenge {
+    
+    private final static int MAX = 65535;
+    
+    private long solve(Map<String, String[]> circuit) {
+        Map<String, Integer> seen = new HashMap<>();
+        return valueOf("a", circuit, seen);
+    }
+    
+    private int valueOf(String wire, Map<String, String[]> circuit, Map<String, Integer> seen) {
+        if (seen.containsKey(wire)) {
+            return seen.get(wire);
+        }
+        
+        String[] wireCalculation = circuit.get(wire);
+        
+        if (wireCalculation == null) {
+            return Integer.parseInt(wire);
+        }
+        
+        if (wireCalculation[0].equals("NOT")) {
+            return MAX ^ valueOf(wireCalculation[1], circuit, seen);
+        }
+        else if (wireCalculation.length == 1) {
+            try {
+                return Integer.parseInt(wireCalculation[0]);
+            }
+            catch (NumberFormatException e) {
+                return valueOf(wireCalculation[0], circuit, seen);
+            }
+        }
+        
+        int result = 0;
+        switch (wireCalculation[1]) {
+            case "AND":
+                result = valueOf(wireCalculation[0], circuit, seen) & valueOf(wireCalculation[2], circuit, seen);
+                break;
+            case "OR":
+                result = valueOf(wireCalculation[0], circuit, seen) | valueOf(wireCalculation[2], circuit, seen);
+                break;
+            case "LSHIFT":
+                result = (valueOf(wireCalculation[0], circuit, seen) << Integer.parseInt(wireCalculation[2])) & MAX;
+                break;
+            case "RSHIFT":
+                result = valueOf(wireCalculation[0], circuit, seen) >> Integer.parseInt(wireCalculation[2]);
+                break;
+        }
+        
+        seen.put(wire, result);
+        return result;
+    }
+
+    @Override
+    public long solve() {
+        Map<String, String[]> circuit = new HashMap<>();
+        
+        Pattern pattern = Pattern.compile("(.*) -> (.*)");
+        
+        Scanner reader = getInputFile();
+        while (reader.hasNextLine()) {
+            Matcher matcher = pattern.matcher(reader.nextLine());
+            matcher.matches();
+            circuit.put(matcher.group(2), matcher.group(1).split(" "));
+        }
+        reader.close();
+        
+        return solve(circuit);
+    }
+
+}
