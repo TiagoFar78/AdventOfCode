4ac114c58956c20d842ec494acabd253f1d5e2a5
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2025-02-13 02:29:01 +0000

    add: 2015 Day 11 (im that good)

---
+++ src/year2015/Day11Part1.java
@@ -0,0 +1,18 @@
+package year2015;
+
+import main.Challenge;
+
+public class Day11Part1 extends Challenge {
+    
+    @Override
+    public String solveString() {        
+        return "Easy to solve by just looking at the input and thinking, code isnt even needed";
+    }
+
+    @Override
+    public long solve() {
+        // Empty
+        return 0;
+    }
+
+}
