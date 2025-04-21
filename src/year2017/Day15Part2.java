b1b367ef8404a2e139048d3e54ff2d4a3c6585f9
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2025-04-22 00:41:56 +0100

    add: 2017 Day 15

---
+++ src/year2017/Day15Part2.java
@@ -0,0 +1,65 @@
+package year2017;
+
+import java.util.Scanner;
+
+import main.Challenge;
+
+public class Day15Part2 extends Challenge {
+    
+    private static final int MOD = 2147483647;
+    private static final int A_FACTOR = 16807;
+    private static final int B_FACTOR = 48271;
+    
+    private int solve(long a, long b, int pairsAmount) {
+        int total = 0;
+        
+        for (int i = 0; i < pairsAmount; i++) {
+            a = a * A_FACTOR % MOD;
+            while (a % 4 != 0) {
+                a = a * A_FACTOR % MOD;
+            }
+            b = b * B_FACTOR % MOD;
+            while (b % 8 != 0) {
+                b = b * B_FACTOR % MOD;
+            }
+            
+            if (match(a, b)) {
+                total++;
+            }
+        }
+        
+        return total;
+    }
+    
+    private boolean match(long a, long b) {
+        int[] aBits = new int[16];
+        int[] bBits = new int[16];
+        
+        for (int i = 0; i < 16; i++) {
+            aBits[i] = (int) (a % 2);
+            bBits[i] = (int) (b % 2);
+            a /= 2;
+            b /= 2;
+        }
+        
+        for (int i = 0; i < 16; i++) {
+            if (aBits[i] != bBits[i]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    @Override
+    public long solve() {
+        Scanner reader = getInputFile();
+        int startingA = Integer.parseInt(reader.nextLine().substring("Generator A starts with ".length()));
+        int startingB = Integer.parseInt(reader.nextLine().substring("Generator B starts with ".length()));
+        reader.close();
+
+        int pairsAmount = 5000000;
+        return solve(startingA, startingB, pairsAmount);
+    }
+
+}
