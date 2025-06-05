d90c25bb3f422d3e16e66af88a869361ac37cad3
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2025-06-05 06:46:39 +0100

    add: 2018 Day 8

---
+++ src/year2018/Day08Part2.java
@@ -0,0 +1,67 @@
+package year2018;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import main.Challenge;
+
+public class Day08Part2 extends Challenge {
+    
+    private int solve(List<Integer> values) {
+        return getSum(values, 0)[0];
+    }
+    
+    private int[] getSum(List<Integer> values, int i) {
+        int sum = 0;
+        int sons = values.get(i);
+        i++;
+        int metadatas = values.get(i);
+        i++;
+        if (sons == 0) {
+            while (metadatas > 0) {
+                sum += values.get(i);
+                metadatas--;
+                i++;
+            }
+            
+            return new int[] { sum, i };
+        }
+        
+        int[] sonsResults = new int[sons];
+        for (int son = 0; son < sons; son++) {
+            int[] res = getSum(values, i);
+            sonsResults[son] = res[0];
+            i = res[1];
+        }
+        
+        while (metadatas > 0) {
+            if (values.get(i) - 1 < sons) {
+                sum += sonsResults[values.get(i) - 1];
+            }
+            metadatas--;
+            i++;
+        }
+        
+        return new int[] { sum, i };
+    }
+    
+    @Override
+    public long solve() {
+        List<Integer> values = new ArrayList<>();
+        
+        Pattern pattern = Pattern.compile("\\d+");
+        
+        Scanner reader = getInputFile();
+        Matcher matcher = pattern.matcher(reader.nextLine());
+        while (matcher.find()) {
+            values.add(Integer.parseInt(matcher.group()));
+        }
+        reader.close();
+        
+        return solve(values);
+    }
+
+}
