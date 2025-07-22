eba43621274397f0d1ce1f1d2efb27c979589164
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2025-07-23 00:18:24 +0100

    add: 2019 Day 21 Part 2

---
+++ src/year2019/Day21Part2.java
@@ -0,0 +1,124 @@
+package year2019;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import main.Challenge;
+
+public class Day21Part2 extends Challenge {
+    
+    private long solve(Map<Long, Long> nums) {
+        long output = 0;
+        
+        List<String> springscriptProgram = new ArrayList<>();
+        springscriptProgram.add("NOT A J");
+        springscriptProgram.add("NOT B T");
+        springscriptProgram.add("AND H T");
+        springscriptProgram.add("OR T J");
+        springscriptProgram.add("NOT C T");
+        springscriptProgram.add("AND H T");
+        springscriptProgram.add("OR T J");
+        springscriptProgram.add("AND D J");
+        springscriptProgram.add("RUN");
+        
+        List<Integer> inputs = new ArrayList<>();
+        for (String instruction : springscriptProgram) {
+            for (char c : instruction.toCharArray()) {
+                inputs.add((int) c);
+            }
+            inputs.add(10);
+        }
+        
+        int input = 0;
+        int relativeBase = 0;
+        for (long i = 0; ; i++) {
+            Long num = nums.getOrDefault(i, 0L);
+            if (num == 99) {
+                break;
+            }
+            
+            int instruction = (int) (num % 10);
+            if (instruction == 1) {
+                nums.put(getAddress(nums, num, i, 3, relativeBase), getParameter(nums, num, i, 1, relativeBase) + getParameter(nums, num, i, 2, relativeBase));
+                i += 3;
+            }
+            else if (instruction == 2) {
+                nums.put(getAddress(nums, num, i, 3, relativeBase), getParameter(nums, num, i, 1, relativeBase) * getParameter(nums, num, i, 2, relativeBase));
+                i += 3;
+            }
+            else if (instruction == 3) {
+                nums.put(getAddress(nums, num, i, 1, relativeBase), (long) inputs.get(input));
+                input++;
+                i += 1;
+            }
+            else if (instruction == 4) {
+                output = getParameter(nums, num, i, 1, relativeBase);
+                System.out.println(output);
+                i += 1;
+            }
+            else if (instruction == 5) {
+                if (getParameter(nums, num, i, 1, relativeBase) != 0) {
+                    i = getParameter(nums, num, i, 2, relativeBase) - 1;
+                }
+                else {
+                    i += 2;
+                }
+            }
+            else if (instruction == 6) {
+                if (getParameter(nums, num, i, 1, relativeBase) == 0) {
+                    i = getParameter(nums, num, i, 2, relativeBase) - 1;
+                }
+                else {
+                    i += 2;
+                }
+            }
+            else if (instruction == 7) {
+                nums.put(getAddress(nums, num, i, 3, relativeBase), getParameter(nums, num, i, 1, relativeBase) < getParameter(nums, num, i, 2, relativeBase) ? 1L : 0L);
+                i += 3;
+            }
+            else if (instruction == 8) {
+                nums.put(getAddress(nums, num, i, 3, relativeBase), getParameter(nums, num, i, 1, relativeBase) == getParameter(nums, num, i, 2, relativeBase) ? 1L : 0L);
+                i += 3;
+            }
+            else if (instruction == 9) {
+                relativeBase += getParameter(nums, num, i, 1, relativeBase);
+                i += 1;
+            }
+        }
+        
+        return output;
+    }
+    
+    private long getParameter(Map<Long, Long> nums, long instruction, long i, int index, long relativeBase) {
+        String s = Long.toString(instruction);
+        int mode = index > s.length() - 2 ? 0 : s.charAt(s.length() - 2 - index) - '0';
+        return mode == 1 ? nums.getOrDefault(i + index, 0L) : mode == 0 ? nums.getOrDefault(nums.getOrDefault(i + index, 0L), 0L) : nums.getOrDefault(relativeBase + nums.getOrDefault(i + index, 0L), 0L);
+    }
+    
+    private long getAddress(Map<Long, Long> nums, long instruction, long i, int index, long relativeBase) {
+        String s = Long.toString(instruction);
+        int mode = index > s.length() - 2 ? 0 : s.charAt(s.length() - 2 - index) - '0';
+        return mode == 0 ? nums.getOrDefault(i + index, 0L) : relativeBase + nums.getOrDefault(i + index, 0L);
+    }
+
+    @Override
+    public long solve() {
+        Map<Long, Long> nums = new HashMap<>();
+        
+        Scanner reader = getInputFile();
+        String[] sopcodes = reader.nextLine().split(",");
+        reader.close();
+        
+        long i = 0;
+        for (String sopcode : sopcodes) {
+            nums.put(i, Long.parseLong(sopcode));
+            i++;
+        }
+        
+        return solve(nums);
+    }
+
+}
