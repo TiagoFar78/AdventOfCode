f08fbf414be163b9f43e45dc0c31f1e028cb989c
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2025-07-30 01:22:02 +0100

    add: 2020 Day 2 Part 1

---
+++ src/year2020/Day02Part1.java
@@ -0,0 +1,72 @@
+package year2020;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import main.Challenge;
+
+public class Day02Part1 extends Challenge {
+    
+    private class PasswordPolicy {
+        
+        private int min;
+        private int max;
+        private char c;
+        private String password;
+        
+        public PasswordPolicy(int min, int max, char c, String password) {
+            this.min = min;
+            this.max = max;
+            this.c = c;
+            this.password = password;
+        }
+        
+    }
+    
+    private int solve(List<PasswordPolicy> passwords) {
+        int valid = 0;
+        for (PasswordPolicy password : passwords) {
+            if (isValid(password)) {
+                valid++;
+            }
+        }
+        
+        return valid;
+    }
+    
+    private boolean isValid(PasswordPolicy password) {
+        int count = 0;
+        for (int i = 0; i < password.password.length(); i++) {
+            if (password.password.charAt(i) == password.c) {
+                count++;
+            }
+        }
+        
+        return count >= password.min && count <= password.max;
+    }
+
+    @Override
+    public long solve() {
+        List<PasswordPolicy> passwords = new ArrayList<>();
+        
+        Pattern pattern = Pattern.compile("(\\d+)-(\\d+) (.): (.*)");
+        
+        Scanner reader = getInputFile();
+        while (reader.hasNextLine()) {
+            Matcher matcher = pattern.matcher(reader.nextLine());
+            matcher.matches();
+            
+            int min = Integer.parseInt(matcher.group(1));
+            int max = Integer.parseInt(matcher.group(2));
+            char c = matcher.group(3).charAt(0);
+            passwords.add(new PasswordPolicy(min, max, c, matcher.group(4)));
+        }
+        reader.close();
+        
+        return solve(passwords);
+    }
+
+}
